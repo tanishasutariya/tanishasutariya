68284cab6693331a6ebdb2951f865178ff8086f5
Sutariya Tanisha <151525536+tanishasutariya@users.noreply.github.com>
2023-12-28 20:09:15 +0530

    Add files via upload

---
+++ 8.3.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+
+main()
+{
+	int r,c;
+	printf("enter row :");
+	scanf("%d",&r);
+	
+	printf("enter column :");
+	scanf("%d",&c);
+	
+	int a[r][c];
+	int i,j;
+	printf("\n\n\t---2-D array input---\n");
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			printf("enter a[%d][%d]=",i,j);
+			scanf("%d",&a[i][j]);
+			}
+		}
+		for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+
+	{
+			
+			int r,c;
+			printf("enter row :");
+			scanf("%d",&r);
+			printf("enter column :");
+			scanf("%d",&c);
+			
+			int a[r][c];
+			int i,j;
+			printf("\n\n\t---boundary elements_---");
+					}
+			for(i=0;i<r;i++)
+	{
+		for(j=0;j<c;j++)
+		{
+			
+			(i==0||j==0||i==r-1||j==c-1);
+			printf("\n");
+		}
+		
+}
+}
+}
+}
+
+
+
